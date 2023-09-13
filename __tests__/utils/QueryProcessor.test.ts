35115926508db3998fd9f3a38ae8482c5107c663
Riya Bhatia <riyab0504@gmail.com>
2023-09-13 12:56:18 -0400

    extended query processor to respond to andrew ID queries, add testing for this

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,10 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test ('should return andrew ID description', () => {
+        const query = "Andrew ID";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe(("I don't know your Andrew ID."));
+    });
 });
